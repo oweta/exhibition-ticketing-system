d8ca5cf788956f4d7aaddde8f80cb9dfe3d22fd0
Oweta Jacob Emmy <oweta001@gmail.com>
2025-05-29 01:43:19 +0300

    Update connection.php

--- database/connection.php
+++ database/connection.php
@@ -1,12 +1,11 @@
 <?php
-$host = 'localhost';      // or 127.0.0.1
+$host = '127.0.0.1';  // <<< instead of 'localhost'
 $db   = 'exhibition_db';
 $user = 'root';
-$pass = '';               // Leave empty for default XAMPP setup
+$pass = '';           // leave blank since phpMyAdmin root has no password
 
 try {
     $conn = new mysqli($host, $user, $pass, $db);
-
     if ($conn->connect_error) {
         throw new Exception("Connection failed: " . $conn->connect_error);
     }
